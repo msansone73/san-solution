7578c0a2bb1c45326c2c8db9ebf8de8ac626cde5
marcio sansone <msansone@gmail.com>
2023-06-15 13:36:24 +0100

    added enviornment variables in docker compose

--- san-gateway/atualizar.sh
+++ san-gateway/atualizar.sh
@@ -2,12 +2,13 @@ echo -------- ATUALIZANDO GIT --------------------
 git pull
 echo -------- MAVEN  --------------------
 mvn package
-echo -------- DESATIVA DOCKER COMPOSE --------------------
-docker-compose down
 echo -------- REMOVE IMAGEM DOCKER --------------------
 docker rmi -f san-gateway-server
 echo -------- CRIA NOVA IMAGEM DOCKER --------------------
 docker build --tag=san-gateway-server:latest .
+echo -------- DESATIVA DOCKER COMPOSE --------------------
+cd ../san-eureka
+docker-compose down
 echo -------- LEVANTA DOCKER COMPOSE --------------------
 docker-compose up --d
 echo -------- CONCLUIDO COM SUCESSO --------------------
--- san-security/atualizar.sh
+++ san-security/atualizar.sh
@@ -2,12 +2,13 @@ echo -------- ATUALIZANDO GIT --------------------
 git pull
 echo -------- MAVEN  --------------------
 mvn package
-echo -------- DESATIVA DOCKER COMPOSE --------------------
-docker-compose down
 echo -------- REMOVE IMAGEM DOCKER --------------------
 docker rmi -f san-security-server
 echo -------- CRIA NOVA IMAGEM DOCKER --------------------
 docker build --tag=san-security-server:latest .
+echo -------- DESATIVA DOCKER COMPOSE --------------------
+cd ../san-eureka
+docker-compose down
 echo -------- LEVANTA DOCKER COMPOSE --------------------
 docker-compose up --d
 echo -------- CONCLUIDO COM SUCESSO --------------------
