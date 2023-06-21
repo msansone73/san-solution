1304ed0b36c9ab708762d161397de25495cd8e7c
marcio sansone <msansone@gmail.com>
2023-06-21 17:04:04 +0100

    Added Email validation on Add User process

--- san-security/src/main/java/br/com/msansone/sansecurity/controller/UserController.java
+++ san-security/src/main/java/br/com/msansone/sansecurity/controller/UserController.java
@@ -11,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.msansone.sansecurity.exceptions.UserExistsException;
 import br.com.msansone.sansecurity.model.User;
+import br.com.msansone.sansecurity.model.dto.ErrorDTO;
 import br.com.msansone.sansecurity.model.dto.UserLoginRequestDTO;
 import br.com.msansone.sansecurity.model.dto.UserResponseDTO;
 import br.com.msansone.sansecurity.service.UserService;
-
 @RestController
 @RequestMapping("/api/security")
 public class UserController {
@@ -25,32 +26,41 @@ public class UserController {
 	
 	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
-	public ResponseEntity<User> addUser(
+	public ResponseEntity<?> addUser(
 			@RequestBody User user){
-		User newUser = userService.save(user);
-		return ResponseEntity.ok(newUser);
+		User newUser=null;
+		UserResponseDTO userDto=null;
+		try {
+			newUser = userService.save(user);
+			userDto= convertUserDTO(newUser, null);
+			
+		} catch (UserExistsException e) {
+			
+			return ResponseEntity.badRequest().body(new ErrorDTO("Email já existe!"));
+		}
+		return ResponseEntity.ok(userDto);
 	}
 	
 	@GetMapping
 	public List<UserResponseDTO> getAll(){
 		List<User> users = userService.getAll();
-		List<UserResponseDTO> usersDTO = users.stream().map(u -> convertUserDTO(u)).toList();
+		List<UserResponseDTO> usersDTO = users.stream().map(u -> convertUserDTO(u,null)).toList();
 		return usersDTO;
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<UserResponseDTO> login(@RequestBody UserLoginRequestDTO useLogin){
+	public ResponseEntity<?> login(@RequestBody UserLoginRequestDTO useLogin){
 		
 		User user = userService.login(useLogin.email(), useLogin.pass());
 
 		if (user==null) {
 			return ResponseEntity.noContent().build();
 		} else {
-			return ResponseEntity.ok(convertUserDTO(user));
+			return ResponseEntity.ok(convertUserDTO(user,null));
 		}
 	}
 
-	private UserResponseDTO convertUserDTO(User user) {
+	private UserResponseDTO convertUserDTO(User user, String errorMessage) {
 		return new UserResponseDTO(user.getId(), 
 				user.getEmail(),
 				user.getName(),
