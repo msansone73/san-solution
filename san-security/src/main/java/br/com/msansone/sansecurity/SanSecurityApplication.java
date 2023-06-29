5778271eb7b01ed9a8c82c8e09c1327eea3f72fd
marcio sansone <msansone@gmail.com>
2023-06-29 20:09:08 +0100

    fix docuker-compose log setup

--- san-security/src/main/java/br/com/msansone/sansecurity/SanSecurityApplication.java
+++ san-security/src/main/java/br/com/msansone/sansecurity/SanSecurityApplication.java
@@ -1,14 +1,20 @@
 package br.com.msansone.sansecurity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import br.com.msansone.saneureka.SanEurekaApplication;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SanSecurityApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(SanSecurityApplication.class);
+		logger.info("Started Eureka.");
 		SpringApplication.run(SanSecurityApplication.class, args);
 	}
 
