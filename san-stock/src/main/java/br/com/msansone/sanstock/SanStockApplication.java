72e76a2cfe8d29bfd84f47383d93faa15e6b1ee5
marcio sansone <msansone@gmail.com>
2023-09-08 13:34:15 +0100

    added san-stock

---
+++ san-stock/src/main/java/br/com/msansone/sanstock/SanStockApplication.java
@@ -0,0 +1,13 @@
+package br.com.msansone.sanstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanStockApplication.class, args);
+	}
+
+}
---
+++ san-stock/src/test/java/br/com/msansone/sanstock/SanStockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msansone.sanstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
